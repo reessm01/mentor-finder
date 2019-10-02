65264c807225e12e3c71742b52134833f462e5d9
Scott Reese <scott_reese@pm.me>
2019-10-02 12:13:30 -0400

    register form redirecting to login as expected and form includes industry

--- mentor_finder/site_user/forms.py
+++ mentor_finder/site_user/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django import forms
 
 from mentor_finder.personality.models import Personality
+from mentor_finder.industry.models import Industry
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=50)
@@ -12,4 +13,5 @@ class RegisterForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
     email = forms.EmailField()
     personality = forms.ModelChoiceField(queryset=Personality.objects.all())
+    industry = forms.ModelChoiceField(queryset=Industry.objects.all())
     is_mentor = forms.BooleanField(initial=False, required=False, label='Sign up as a mentor.')
